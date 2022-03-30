0491112c28a22d2ff25181d7bd5533fd26fdb44d
foramkavathiya <foramkavathiya@gmail.com>
2022-03-30 15:44:27 -0400

    fixing bug

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -27,14 +27,14 @@ const TuitStats = ({tuit, dislikeTuit, likeTuit = () => {}}) => {
         <div className="col">
           <span onClick={() => dislikeTuit(tuit)}>
                 {
-                    tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
+                    tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes > 0 &&
                     <i className="fas fa-thumbs-down me-1" style={{color: 'blue'}}></i>
                 }
                 {
-                    tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                    tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes <= 0 &&
                     <i className="far fa-thumbs-down me-1"></i>
                 }
-            {tuit.stats && tuit.stats.likes}
+            {tuit.stats && tuit.stats.dislikes}
           </span>
         </div>
         <div className="col">
