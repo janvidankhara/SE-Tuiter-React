0eeac9a233b538bf6733b7095e4e406ce2d483bd
foramkavathiya <foramkavathiya@gmail.com>
2022-03-30 15:38:41 -0400

    fixing bug

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
+const TuitStats = ({tuit, dislikeTuit, likeTuit = () => {}}) => {
     return (
       <div className="row mt-2">
         <div className="col">
@@ -25,16 +25,16 @@ const TuitStats = ({tuit, likeTuit, dislikeTuit = () => {}}) => {
           </span>
         </div>
         <div className="col">
-                <span onClick={() => dislikeTuit(tuit)}>
+          <span onClick={() => dislikeTuit(tuit)}>
                 {
-                    tuit.stats > 0 &&
+                    tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
                     <i className="fas fa-thumbs-down me-1" style={{color: 'blue'}}></i>
                 }
-                    {
-                        tuit.stats  <= 0 &&
-                        <i className="far fa-thumbs-down me-1"></i>
-                    }
-                    {tuit.stats && tuit.stats.dislikes}
+                {
+                    tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                    <i className="far fa-thumbs-down me-1"></i>
+                }
+            {tuit.stats && tuit.stats.dislikes}
           </span>
         </div>
         <div className="col">
