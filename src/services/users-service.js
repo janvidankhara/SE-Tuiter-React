38e41fb052d114777679ab548efd8839a18c7ad6
foramkavathiya <foramkavathiya@gmail.com>
2022-04-20 12:44:25 -0400

    Updated

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL = "https://se-tuiter-project-back-end.herokuapp.com";
 //"http://localhost:4000";
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+//const BASE_URL = process.env.REACT_APP_BASE_URL;
 const USERS_API = `${BASE_URL}/api/users`;
 
 export const createUser = (user) =>
